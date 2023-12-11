eb5998e8967b644c710a153a951cf5ddf7fdae38
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-12-11 22:09:25 +0500

    add files

---
+++ script.js
@@ -0,0 +1,213 @@
+// console.log("hello world");
+
+// arithmetic operators
+// + , - , * , /
+
+// let a = 15;
+// let b = 7;
+
+// console.log("a =", a, "& b =", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);   // modules
+// console.log("a ** b =", a ** b);  // exponentiation
+
+
+// Unary operators
+
+// let a = 15;
+// let b = 7;
+
+// console.log("a =", a, "& b =", b);
+// a--;
+// // a++;   // increment
+// // --a;   // Decrement
+// // ++a;
+// console.log("a =", a);
+
+
+// Assignment operators
+
+// let a = 15;
+// let b = 7;
+
+// console.log("a =", a, "& b =", b);
+
+// // a += 4 ;  // means that a = a + 4
+// // a -= 4 ;  // means that a = a - 4
+// // a *= 4 ;  // means that a = a * 4
+// // a /= 4 ;  // means that a = a / 4
+// // a %= 4 ;  // means that a = a % 4
+// // a **= 4 ;  // means that a = a ** 4
+
+// console.log("a =", a);
+
+// Comparison operators
+
+// let a = 15;
+// let b = 7;
+
+// console.log("a =", a, "& b =", b);
+
+// console.log(" a == b", a == b);
+// console.log(" a != b", a != b);
+// console.log(" a === b", a === b);
+// console.log(" a !== b", a !== b);
+
+// console.log(" a < b", a < b);
+// console.log(" a <= b", a <= b);
+// console.log(" a > b", a > b);
+// console.log(" a >= b", a >= b);
+
+
+// Logical operators
+
+// let a = 15;
+// let b = 7;
+
+// console.log("a =", a, "& b =", b);
+
+// let cond1 = a > b;
+// // let cond2 = a == b;
+
+// // console.log(" cond1 && cond2 ", cond1 && cond2);
+// // console.log(" cond1 || cond2 ", cond1 || cond2);
+// console.log(" !(a>b) ", ! (a>b));
+
+
+// Conditional Statements
+
+// 1--->  if Statement
+
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("You Can Vote");
+// }
+
+// if (age < 18) {
+//     console.log("You CANNOT Vote");
+// }
+
+// let mode = "blue";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// if (mode === "blue") {
+//     color = "White";
+// }
+
+// console.log(color);
+
+
+// let message = " Asalam Alaikam "
+
+// if (message === " Asalam Alaikam") {
+//     console.log("Shahbash Betay boht barhiya");
+// }
+
+// if (message === " Hello ") {
+//     console.log("Bhai hello nahi kehtay ");
+// }
+
+// let age = 15 ;
+
+// if ( age >= 20) {
+//     console.log("phudi dya vote paya kr");
+// }
+
+// if ( age < 20) {
+//     console.log("Abhi tum bachay ho");
+// }
+
+
+// 2--->  if-else Statement
+
+// let age = 15;
+
+// if (age >= 18) {
+//     console.log("You can Vote ");
+// }
+
+// else {
+//     console.log("You CANNOT Vote ");
+// }
+
+// let num = 999;
+
+// if (num % 2 === 0) {
+//     console.log(num, "is even");
+// }
+
+// else {
+//     console.log(num, "is Odd");
+
+// }
+
+// 3--->  else-if Statement
+
+// let mode = "pink";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// }
+
+// else if (mode === "blue") {
+//     color = "blue";
+// }
+
+// else if (mode === "pink") {
+//     color = "pink";
+// }
+
+// else {
+//     color = "White";
+// }
+
+// console.log(color);
+
+
+// alert("Hello World")
+// let num = prompt("Hello World");
+
+// Excercise no 1
+
+// let num = prompt("Enter a number");
+
+// if (num % 5 === 0) {
+//     console.log(num, "is multipe of 5");
+// }
+
+// else  {
+//     console.log(num, "is not multipe of 5");
+// }
+
+// Excercise no 2
+
+// let score = 75;
+
+// if (score >= 90 && score <= 100) {
+//     console.log("A");
+// }
+
+// else if (score >= 80 && score <= 90) {
+//     console.log("B");
+// }
+// else if (score >= 70 && score <= 80) {
+//     console.log("C");
+// }
+// else if (score >= 60 && score <= 70) {
+//     console.log("D");
+// }
+// else {
+//     console.log("Fail");
+// }
+
+
+
