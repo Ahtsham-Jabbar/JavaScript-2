cde9804ebc6d0efe5df708d18d7039c86a411628
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-12-12 12:03:58 +0500

    change

---
+++ practise.js
@@ -0,0 +1,9 @@
+let num = prompt("Enter a number");
+
+if (num % 5 === 0) {
+    console.log(num, "Num is multiple of 5");
+}
+
+else {
+    console.log(num, "Num is NOT multiple of 5");
+}
--- script.js
+++ script.js
@@ -190,24 +190,24 @@
 
 // Excercise no 2
 
-let score = 75;
+// let score = 75;
 
-if (score >= 90 && score <= 100) {
-    console.log("A");
-}
+// if (score >= 90 && score <= 100) {
+//     console.log("A");
+// }
 
-else if (score >= 80 && score <= 90) {
-    console.log("B");
-}
-else if (score >= 70 && score <= 80) {
-    console.log("C");
-}
-else if (score >= 60 && score <= 70) {
-    console.log("D");
-}
-else {
-    console.log("Fail");
-}
+// else if (score >= 80 && score <= 90) {
+//     console.log("B");
+// }
+// else if (score >= 70 && score <= 80) {
+//     console.log("C");
+// }
+// else if (score >= 60 && score <= 70) {
+//     console.log("D");
+// }
+// else {
+//     console.log("Fail");
+// }
 
 
 
